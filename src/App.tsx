a09e9913a50ed5d1f6e512477087072c31b896f8
fukumoto <fukumoto@axcel-mode.co.jp>
2025-07-14 14:38:26 +0900

    React環境を構築

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ジロウティア</h1>
+        <p>ラーメン屋さんのTierリスト</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
